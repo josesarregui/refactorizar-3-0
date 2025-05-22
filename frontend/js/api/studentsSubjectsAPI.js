eb4064233615abf3d20a023058680de7462de467
gabrielinuz <gabrielinuz@gmail.com>
2025-05-21 21:55:22 -0300

    orden de comentarios

--- frontend/js/api/studentsSubjectsAPI.js
+++ frontend/js/api/studentsSubjectsAPI.js
@@ -1,34 +1,29 @@
-import { createAPI } from './apiFactory.js';
-export const studentsSubjectsAPI = createAPI('studentsSubjects');
+// import { createAPI } from './apiFactory.js';
+// export const studentsSubjectsAPI = createAPI('studentsSubjects');
 
 /**
  * Ejemplo de extensión de la API:
-
-import { createAPI } from './apiFactory.js';
-
-const baseAPI = createAPI('studentsSubjects');
-
-export const studentsSubjectsAPI = 
-{
-    ...baseAPI, // hereda fetchAll, create, update, remove
-
-    // método adicional personalizado
-    async fetchByStudentId(id) 
-    {
-        const res = await fetch(`../../backend/server.php?module=studentsSubjects&studentId=${id}`);
-        if (!res.ok) throw new Error("No se pudieron obtener asignaciones del estudiante");
-        return await res.json();
-    }
-};
-
 */
+// import { createAPI } from './apiFactory.js';
+// const baseAPI = createAPI('studentsSubjects');
+
+// export const studentsSubjectsAPI = 
+// {
+//     ...baseAPI, // hereda fetchAll, create, update, remove
+
+//     // método adicional personalizado
+//     async fetchByStudentId(id) 
+//     {
+//         const res = await fetch(`../../backend/server.php?module=studentsSubjects&studentId=${id}`);
+//         if (!res.ok) throw new Error("No se pudieron obtener asignaciones del estudiante");
+//         return await res.json();
+//     }
+// };
 
 /**
  * También permite url personalizadas ahora:
-
-const customAPI = createAPI('custom', 
-{
-    urlOverride: '../../backend/misRutas/personalizadas.php'
-});
-
- */
+*/
+// const customAPI = createAPI('custom', 
+// {
+//     urlOverride: '../../backend/misRutas/personalizadas.php'
+// });
